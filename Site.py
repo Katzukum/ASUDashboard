6ef419a02e0ef0999b0bde1304da3287f6c5acad
Katzukum <katzukum@gmail.com>
2022-07-25 22:43:49 -0400

    Initial

--- Site.py
+++ Site.py
@@ -1,8 +1,19 @@
 import streamlit as st
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+import pandas as pd
 
+cred = credentials.Certificate("asukey.json")
+firebase_admin.initialize_app(cred)
 
+db = firestore.client()
+
+membersCollection = db.collection(u'members').stream()
+membersDF = pd.DataFrame.from_dict(membersCollection)
 
 st.title('AfterShock East Greed Data Hub')
 
 st.text_input('SearchText',value='Search Greed')
+st.table(membersDF)
 
